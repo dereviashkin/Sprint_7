56701c5018e27c7134ab9094581498aba70aade4
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-15 19:17:01 +0300

    endpoint class

---
+++ src/test/java/data/Endpoints.java
@@ -0,0 +1,9 @@
+package data;
+
+public class Endpoints {
+    private static final String courierLogin = "api/v1/courier/login";
+
+    public static String getCourierLoginEndpoint() {
+        return courierLogin;
+    }
+}
