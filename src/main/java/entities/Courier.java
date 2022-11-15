c00bdbc6f559cf8e30c98ea71ed770494c892e5a
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-16 02:09:58 +0300

    Courier.java added setters and 2 param constructor

--- src/main/java/entities/Courier.java
+++ src/main/java/entities/Courier.java
@@ -1,13 +1,27 @@
 package entities;
 
 public class Courier {
-    private final String login;
-    private final String password;
-    private final String name;
+    private String login;
+    private String password;
+    private String name;
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public Courier(String login, String password, String name) {
         this.login = login;
         this.password = password;
         this.name = name;
     }
+
+    public Courier(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
 }
