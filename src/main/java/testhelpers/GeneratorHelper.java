4b46226ad5b86dd31d96cd3ec3ccac9900548a88
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-16 02:11:07 +0300

    GeneratorHelper.java added step annotation

--- src/main/java/testhelpers/GeneratorHelper.java
+++ src/main/java/testhelpers/GeneratorHelper.java
@@ -1,9 +1,13 @@
 package testhelpers;
 
+import io.qameta.allure.Step;
+
 import java.util.UUID;
 
 public class GeneratorHelper {
-    public static String stringGenerator(int length){
+
+    @Step("Generating random string of length")
+    public static String stringGenerator(int length) {
         return UUID.randomUUID().toString().replace("-", "").substring(0, length);
     }
 }
