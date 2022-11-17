b98ca3aa3cb09c3aaa56d273250f322df310f6c9
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-18 01:23:45 +0300

    Orders.java

---
+++ src/main/java/entities/Orders.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Orders {
+    private static String firstName;
+    private static String lastName;
+    private static String address;
+    private static String metroStation;
+    private static String phone;
+    private static Integer rentTime;
+    private static String deliveryDate;
+    private static String comment;
+    private static String[] color;
+}
