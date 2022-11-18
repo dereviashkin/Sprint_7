1627d80a289922cc80fb13d88db053dca98157c8
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-18 18:37:12 +0300

    Random number generator

--- src/main/java/testhelpers/GeneratorHelper.java
+++ src/main/java/testhelpers/GeneratorHelper.java
@@ -3,11 +3,18 @@ package testhelpers;
 import io.qameta.allure.Step;
 
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GeneratorHelper {
 
     @Step("Generating random string of length")
     public static String stringGenerator(int length) {
         return UUID.randomUUID().toString().replace("-", "").substring(0, length);
+    }
+
+    @Step("Generating random number of length")
+    public static Integer numberGenerator(int bound) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        return random.nextInt(1, bound);
     }
 }
