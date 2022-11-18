a3dfd0643d1d4151e48f10cfaf4ee0e97d26da29
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-18 18:36:56 +0300

    Order endpoint

--- src/main/java/data/Endpoints.java
+++ src/main/java/data/Endpoints.java
@@ -3,6 +3,7 @@ package data;
 public class Endpoints {
     private static final String courierCreate = "api/v1/courier/";
     private static final String courierLogin = courierCreate + "login";
+    private static final String ordersCreate = "/api/v1/orders";
 
     public static String getCourierCreateEndpoint() {
         return courierCreate;
@@ -10,5 +11,9 @@ public class Endpoints {
 
     public static String getCourierLoginEndpoint() {
         return courierLogin;
+    }
+
+    public static String getOrdersCreateEndpoint() {
+        return ordersCreate;
     }
 }
