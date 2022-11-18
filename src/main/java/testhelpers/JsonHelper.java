d84ae382fbfcf8c9dd919d04376b3e0f6556840d
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-18 20:53:14 +0300

    new methods

--- src/main/java/testhelpers/JsonHelper.java
+++ src/main/java/testhelpers/JsonHelper.java
@@ -1,21 +1,32 @@
 package testhelpers;
 
-import entities.Courier;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
+import org.junit.Assert;
 
+import static data.Endpoints.getCourierCreateEndpoint;
+import static data.Endpoints.getOrdersCancelEndpoint;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static testhelpers.CourierHelper.getIdCourier;
+import static testhelpers.CourierHelper.setIdCourier;
+import static testhelpers.OrderHelper.getIdOrder;
+import static testhelpers.OrderHelper.setIdOrder;
 
 public class JsonHelper {
 
     @Step("Send POST request to required endpoint")
-    public static Response sendPostRequestEndpoint(Courier courier, String endpoint) {
+    public static Response sendPostRequestEndpoint(Object serializedJson, String endpoint) {
         return given()
                 .header("Content-type", "application/json")
-                .body(courier)
+                .body(serializedJson)
                 .post(endpoint);
+    }
+
+    @Step("Send GET request to required endpoint")
+    public static Response sendGetRequestEndpoint(String endpoint) {
+        return given()
+                .header("Content-type", "application/json")
+                .get(endpoint);
     }
 
     @Step("Check if response code is expected")
@@ -23,23 +34,60 @@ public class JsonHelper {
         response.then().statusCode(code);
     }
 
-    @Step("Check if response value is not null")
-    public static void validateResponseValueNotNull(Response response, String key) {
-        response.then().assertThat().body(key, notNullValue());
+    @Step("Check if response id value is not null")
+    public static void validateResponseIdNotNull(Response response) {
+        Assert.assertNotNull(getValueByKey(response, "id"));
+        setIdCourier(Integer.valueOf(getValueByKey(response, "id")));
     }
 
-    @Step("Check if response value is expected")
-    public static void validateResponseValue(Response response, String key, String value) {
-        response.then().assertThat().body(key, equalTo(value));
+    @Step("Check if response orders value is not null")
+    public static void validateResponseOrdersNotNull(Response response) {
+        Assert.assertNotNull(getValueByKey(response, "orders"));
     }
 
-    @Step("Check if response boolean value is expected")
-    public static void validateResponseValueBoolean(Response response, String key, boolean value) {
-        response.then().assertThat().body(key, equalTo(value));
+    @Step("Check if response track value is not null")
+    public static void validateResponseTrackNotNull(Response response) {
+        Assert.assertNotNull(getValueByKey(response, "track"));
+        setIdOrder(Integer.valueOf(getValueByKey(response, "track")));
+    }
+
+    @Step("Check if response message value is expected")
+    public static void validateResponseMessageValue(Response response, String value) {
+        Assert.assertEquals(getValueByKey(response, "message"), value);
+    }
+
+    @Step("Check if response boolean ok value is expected")
+    public static void validateResponseOkValueIsTrue(Response response) {
+        Assert.assertEquals(getValueByKey(response, "ok"), "true");
     }
 
     @Step("Print response body to console")
     public static void printResponseBodyToConsole(Response response) {
         response.prettyPrint();
+    }
+
+    @Step("Get value by key")
+    public static String getValueByKey(Response response, String key) {
+        return response.then().extract().body().path(key).toString();
+    }
+
+    @Step("Remove courier and order")
+    public static void removeCourierAndOrderById() {
+        if (getIdOrder() != 0) {
+            given()
+                    .header("Content-type", "application/json")
+                    .put(getOrdersCancelEndpoint() + "?track=" + getIdOrder());
+            System.out.println("Deleted order id: " + getIdOrder());
+            System.out.println();
+            setIdOrder(0);
+        }
+        if (getIdCourier() != 0) {
+            given()
+                    .header("Content-type", "application/json")
+                    .delete(getCourierCreateEndpoint() + "/" + getIdCourier());
+            System.out.println("Deleted courier id: " + getIdCourier());
+            System.out.println();
+            setIdCourier(0);
+        }
     }
 }
