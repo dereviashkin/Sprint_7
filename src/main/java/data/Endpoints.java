a4ab7f9de51b24061ab0fe81cfc6b12766072c76
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-18 20:52:59 +0300

    new endpoints

--- src/main/java/data/Endpoints.java
+++ src/main/java/data/Endpoints.java
@@ -1,9 +1,10 @@
 package data;
 
 public class Endpoints {
-    private static final String courierCreate = "api/v1/courier/";
-    private static final String courierLogin = courierCreate + "login";
-    private static final String ordersCreate = "/api/v1/orders";
+    private static final String courierCreate = "api/v1/courier";
+    private static final String courierLogin = courierCreate + "/login";
+    private static final String ordersCreate = "api/v1/orders";
+    private static final String ordersCancel = ordersCreate + "/cancel";
 
     public static String getCourierCreateEndpoint() {
         return courierCreate;
@@ -15,5 +16,9 @@ public class Endpoints {
 
     public static String getOrdersCreateEndpoint() {
         return ordersCreate;
+    }
+
+    public static String getOrdersCancelEndpoint() {
+        return ordersCancel;
     }
 }
