2f446a112151d81603c9308ed69c0aef6ffcc8e6
Валаев Георгий <ymnikgoga@gmail.com>
2022-11-21 17:56:01 +0300

    Инкапсуляция тестового метода

--- src/test/java/tests/CreateOrderTest.java
+++ src/test/java/tests/CreateOrderTest.java
@@ -3,25 +3,19 @@ package tests;
 import entities.Orders;
 import io.qameta.allure.Description;
 import io.qameta.allure.TmsLink;
-import io.restassured.response.Response;
-import org.apache.http.HttpStatus;
 import org.junit.Test;
 
-import static data.Endpoints.getOrdersCreateEndpoint;
-import static testhelpers.JsonHelper.*;
-import static testhelpers.OrderHelper.printOrderData;
-import static testhelpers.OrderHelper.randomOrder;
+import static testhelpers.JsonHelper.sendPostCreateOrderEndpoint;
+import static testhelpers.OrdersHelper.printOrderData;
+import static testhelpers.OrdersHelper.randomOrder;
 
 public class CreateOrderTest extends BaseTest {
     @Test
     @Description("Создание заказа")
-    @TmsLink("TestCase-CreateOrder-4")
+    @TmsLink("TestCase-CreateOrder-1")
     public void createCorrectOrder() {
         Orders randomOrder = randomOrder();
-        Response response = sendPostRequestEndpoint(randomOrder, getOrdersCreateEndpoint());
-        validateResponseCode(response, HttpStatus.SC_CREATED);
         printOrderData(randomOrder);
-        validateResponseTrackNotNull(response);
-        printResponseBodyToConsole(response);
+        sendPostCreateOrderEndpoint(randomOrder);
     }
 }
